8dc432c032884a2374c517f6bc0f43a350100d0b
guswjd2103 <44867153+guswjd2103@users.noreply.github.com>
2020-01-21 21:00:35 +0900

    apply design

--- frontend/week4/src/components/showFileList.js
+++ frontend/week4/src/components/showFileList.js
@@ -16,7 +16,8 @@ class ShowFileList extends Component {
     }
     
     
-    handleOnClick(subject) {
+    handleOnClick(subject) {   
+        console.log(subject);
         this.setState({
             subject : subject
         })
@@ -26,32 +27,15 @@ class ShowFileList extends Component {
     render() {
         return (
             <div>
-                <div class='file-container'>
-                    <div class='sidebar'>
-                        {this.props.list.map((file,index)=>(
-                            
-                                    <div class='navigation-elm' key={index}><span class='nav-text'><i class="fa fa-fw fa-area-chart"></i>
-                                    <span class='text-short' onClick={()=>this.handleOnClick(file.subject)}>{file.subject}</span>
-                                    <div></div>
-                                    <i class="fa fa-fw fa-area-chart"></i><span class='text-long'>{file.professor}</span>
-                                        </span></div>
-                        ))}
-                    </div>
+                <div class='sidebar'>
+                    {this.props.list.map((file,index)=>(
+                        <div class='navigation-elm' key={index}><span class='nav-text'><i class="fa fa-fw fa-area-chart"></i>
+                        <span class='text-short' onClick={()=>this.handleOnClick(file.subject)}>{file.subject}</span>
+                        <div></div>
+                        <i class="fa fa-fw fa-area-chart"></i><span class='text-long'>{file.professor}</span>
+                            </span></div>
+                    ))}
                 </div>
-                
-                <div class="card">
-                    <div class="card__header">
-                    <div id="lineB-ChartExample"></div>
-                    </div>
-                    <div class="card__body">
-                    <h4>Daily Sales</h4>
-                    <p>Lorem ipsum dolor sit.</p>
-                    </div>
-                    <div class="card__footer">
-                    <i class="material-icons">update</i><a href="">Updated 4mins ago</a>
-                    </div>
-                </div>
-
 
                 <div>
                     {this.state.subject ? <FileList subject = {this.state.subject}/> : null}
